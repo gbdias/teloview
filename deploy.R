142ec2bc4220e972bd8f44eb08ad408b8b75e902
Guilherme Borges Dias <guilhermebdias@live.com>
2023-09-08 10:51:14 +0200

    .

--- deploy.R
+++ deploy.R
@@ -5,5 +5,4 @@ setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
                token = Sys.getenv("TOKEN"),
                secret = Sys.getenv("SECRET"))
 # Deploy
-deployApp(appFiles = c("ui.R", "server.R", "likes.rds"))
-
+deployApp(appFiles = c("teloview.R"))
